0d277de0381285270b25906037eaa928c3e6ba9c
Chris Siddall <Chris.Siddall@cpplc.com>
2021-08-23 16:56:34 +0100

    Successfully working. Post message from postman to HTTP trigger function app, writes name to my test queue.

--- Function1.cs
+++ Function1.cs
@@ -7,11 +7,18 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
 
 namespace TestServiceBusFunctionApp
 {
     public static class Function1
     {
+        //sidtodo: DO NOT CHECK IN THE CONNECTION STRING!
+        //static string _connectionString = "Endpoint=sb://chrissiddall.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/RTPPRYoMn4gpLtixGWF+eE93WAaePQp73NY8MHE0mU=";
+        static string _connectionString = "..";
+
+        static string _queueName = "TestQueue";
+
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
@@ -23,13 +30,32 @@ namespace TestServiceBusFunctionApp
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name = name ?? data?.name ?? "noname";
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            ///////////////
+
+            string responseMessage = "";
+            try
+            {
+                await using (var sbClient = new ServiceBusClient(_connectionString))
+                await using (var sender = sbClient.CreateSender(_queueName))
+                using (var messageBatch = await sender.CreateMessageBatchAsync())
+                {
+
+                    messageBatch.TryAddMessage(new ServiceBusMessage(name));
+                    await sender.SendMessagesAsync(messageBatch);
+
+                    responseMessage = $"{name} added to the queue successfully.";
+
+                }
+            }
+            catch (Exception e)
+            {
+                responseMessage = $"Failed to addd {name} to the queue: {e.ToString()}";
+            }
 
             return new OkObjectResult(responseMessage);
+
         }
     }
 }
